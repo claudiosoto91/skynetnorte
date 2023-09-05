a8ff11edc29137e99587dea96db3bbff0ed50664
Claudio <claudio91soto@gmail.com>
2023-09-05 01:10:48 -0300

    agregando modales

--- js/app.js
+++ js/app.js
@@ -9,60 +9,31 @@ window.addEventListener('scroll', () =>{
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-
-    const btnAbrirModal = document.getElementById("btnAbrirModal");
-    const btnCerrarModal = document.getElementById("btnCerrarModal");
-    const modal = document.getElementById("miModal");
-
-    btnAbrirModal.addEventListener("click", function () {
-        modal.style.display = "block"; // Mostrar el modal
-    });
-
-    btnCerrarModal.addEventListener("click", function () {
-        modal.style.display = "none"; // Ocultar el modal
-    });
+    Fancybox.bind('[data-fancybox]', {
+        // Custom options for all galleries
+      });  
+      Fancybox.bind('[data-fancybox="gallery-a"]', {
+        // Custom options for the first gallery
+      });    
     
-    let indiceImagenActual = 0;
-    const imagenes = document.querySelectorAll(".galeria img");
+      Fancybox.bind('[data-fancybox="gallery-b"]', {
+        // Custom options for the second gallery
 
-    function mostrarImagen(indice) {
-        imagenes[indiceImagenActual].style.display = "none"; // Ocultar imagen actual
-        indiceImagenActual = (indice + imagenes.length) % imagenes.length; // Calcular el nuevo índice de imagen
-        imagenes[indiceImagenActual].style.display = "block"; // Mostrar nueva imagen
-    }
+      }); 
 
-    // Funciones para los botones de navegación
-    function cambiarImagen(n) {
-        mostrarImagen(indiceImagenActual + n);
-    }
+      Fancybox.bind('[data-fancybox="gallery"]', {
+        compact: false,
+        Carousel: {
+          Navigation : false
+        },
+        Toolbar: {
+          display: {
+            left: [],
+            middle: ["prev", "infobar", "next"],
+            right: ["close"],
+          }
+        }
+      });  
 
-    // Mostrar primera imagen al abrir el modal
-    mostrarImagen(indiceImagenActual);
-
-    // ... tu código anterior ...
-
-    // Eventos para los botones de navegación
-    const botonAnterior = document.querySelector(".anterior");
-    const botonSiguiente = document.querySelector(".siguiente");
-
-    botonAnterior.addEventListener("click", function () {
-        cambiarImagen(-1);
-    });
-
-    botonSiguiente.addEventListener("click", function () {
-        cambiarImagen(1);
-    });
 });
-Fancybox.bind('[data-fancybox]', {
-    // Custom options for all galleries
-  });  
-  Fancybox.bind('[data-fancybox="gallery-a"]', {
-    // Custom options for the first gallery
-  });    
-
-  Fancybox.bind('[data-fancybox="gallery-b"]', {
-    // Custom options for the second gallery
-    Carousel : {
-      infinite: false
-    }
-  });  
+ 
